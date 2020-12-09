170a9560f2040f3020d7855390b38934b6015d00
joseph-emmanuel <joseph.emmanuel@outlook.com>
2020-12-09 14:10:09 -0500

    completed functions for both timestamp and add contacts

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,11 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Create page</h1>
-</body>
-</html>
+@extends('contacts.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Contact</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('contacts.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('contacts.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Full name:</strong>
+                <input type="text" name="contact_name" class="form-control" placeholder="Full Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Designation:</strong>
+                <input type="text" name="contact_designation" class="form-control" placeholder="Designation">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Company Name:</strong>
+                <input type="text" name="contact_company_name" class="form-control" placeholder="Company Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Email:</strong>
+                <input type="text" name="contact_email" class="form-control" placeholder="Email">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Phone Number</strong>
+                <input type="text" name="contact_phone_number" class="form-control" placeholder="Phone number">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Address</strong>
+                <input type="text" name="contact_address" class="form-control" placeholder="Address">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
