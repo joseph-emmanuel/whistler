f8759ea6d4c9c5564f6090ebe47dcb3d78b77337
joseph-emmanuel <joseph.emmanuel@outlook.com>
2020-12-14 13:26:43 -0500

    updated the blades

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -1,11 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Edit page</h1>
-</body>
-</html>
+@extends('contacts.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Contact</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('contacts.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('contacts.update',$contact->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Full name:</strong>
+                <input type="text" value="{{ $contact->contact_name }}" name="contact_name" class="form-control" placeholder="Full Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Designation:</strong>
+                <input type="text" value="{{ $contact->contact_designation }}" name="contact_designation" class="form-control" placeholder="Designation">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Company Name:</strong>
+                <input type="text" name="contact_company_name" value="{{ $contact->contact_company_name }}" class="form-control" placeholder="Company Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Email:</strong>
+                <input type="text" name="contact_email" value="{{ $contact->contact_email }}" class="form-control" placeholder="Email">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Contact Number</strong>
+                <input type="text" name="contact_phone_number" value="{{ $contact->contact_phone_number }}" class="form-control" placeholder="Phone number">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Address</strong>
+                <input type="text" name="contact_address" value="{{ $contact->contact_address }}" class="form-control" placeholder="Address">
+            </div>
+        </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
