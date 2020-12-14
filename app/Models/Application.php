d200a858a27a02c5ded07f330fc1ade09dfc982d
joseph-emmanuel <joseph.emmanuel@outlook.com>
2020-12-14 14:49:48 -0500

    completed model for the application

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Application extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'status','application_date',
+        'company_name',
+         'contact_name',
+         'phone',
+         'email',
+         'linkedin_name',
+         'Address',
+        'job_title',
+        'job_description',
+        'technologies',
+         'reference_number',
+         'salary',
+         'ending_date',
+        'source',
+        'fit_value',
+        'resume',
+        'cover_letter',
+        'transcript',
+        'reference_document',
+        'notes'
+    ];
 }
